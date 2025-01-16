a788d7c96bee0b0b21e0609344cfe463a9be75a9
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-01-16 12:20:17 +0530

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,11 +5,15 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { tokenCache } from '@/utils/cache';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -28,6 +32,9 @@ export default function RootLayout() {
   }
 
   return (
+    <ClerkProvider publishableKey={publishableKey}
+      tokenCache={tokenCache}
+    >
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -35,5 +42,6 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+    </ClerkProvider>
   );
 }
